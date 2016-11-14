d62e150cdaad15904ce4d3ffadf789e8059e81f3
WeersProductions <weersproductions@gmail.com>
2016-11-14 16:50:18 +0100

    even more 1.8

--- src/main/AutoDealer.java
+++ src/main/AutoDealer.java
@@ -1,10 +1,12 @@
 package main;
 
 public class AutoDealer extends Car{
-	public AutoDealer(float price, int distributionTimes) {
+	public AutoDealer(float price, int distributionTimes, Person latestOwner) {
 		super(price);
 		this.distributionTimes = distributionTimes;
+		this.latestOwner = latestOwner;
 	}
 
 	private int distributionTimes;
+	private Person latestOwner;
 }
