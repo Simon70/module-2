3bf19984ec9aaac32de6ddf348758113ca3def32
Simon Struck <simonstruck70@gmail.com>
2016-11-14 16:47:44 +0100

    Added to 1.8

---
+++ src/main/Customer.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Date;
+
+/**
+ * Created by simon on 14.11.16.
+ */
+public class Customer extends Person{
+    private Video[] borrowedVideos;
+
+    public Customer() {
+        this.borrowedVideos = new Video[1];
+    }
+}
