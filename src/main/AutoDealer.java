2d923ef59c1a1cd964eb65c71b4c5d6ddee46cee
WeersProductions <weersproductions@gmail.com>
2016-11-14 16:58:33 +0100

    Fixed AutoDealer.java constructor

--- src/main/AutoDealer.java
+++ src/main/AutoDealer.java
@@ -1,8 +1,8 @@
 package main;
 
-public class AutoDealer extends Car{
-	public AutoDealer(float price, int distributionTimes, Person latestOwner) {
-		super(price);
+public class AutoDealer extends Car {
+	public AutoDealer(float price, int distributionTimes, Person latestOwner, Manufacturer manufacturer) {
+		super(price, manufacturer);
 		this.distributionTimes = distributionTimes;
 		this.latestOwner = latestOwner;
 	}
