4c1d781915a443c81648c32751db8384a1d58891
WeersProductions <weersproductions@gmail.com>
2016-11-16 14:25:43 +0100

    Finished Dollars.java

--- src/main/Dollars.java
+++ src/main/Dollars.java
@@ -1,20 +1,49 @@
 package main;
 
-
 public class Dollars {
-public int cents() {
-	
-}
 
-public void add(int dollars, int cents){
-	
-}
+	private int	dollars;
+	private int	cents;
 
-public void reset() {
-	
-}
+	public int dollars() throws Exception {
+		if (this.dollars >= 0) {
+			return this.dollars;
+		} else {
+			throw new Exception("Dollars is negative");
+		}
+	}
 
-public void substract(int dollars, int cents) {
-	
-}
+	public int cents() throws Exception {
+		if (this.cents < 0) {
+			throw new Exception("Cents are negative");
+		} else if (this.cents > 99) {
+			throw new Exception("Cents is too big");
+		}
+		return this.cents;
+	}
+
+	public void add(int dollars, int cents) {
+		this.dollars += dollars;
+		this.cents += cents;
+
+		while (this.cents > 100) {
+			this.dollars += 1;
+			this.cents -= 100;
+		}
+	}
+
+	public void reset() {
+		this.dollars = 0;
+		this.cents = 0;
+	}
+
+	public void substract(int dollars, int cents) {
+		this.dollars -= dollars;
+		this.cents -= cents;
+		
+		while (this.cents < 0) {
+			this.dollars -= 1;
+			this.cents += 100;
+		}
+	}
 }
