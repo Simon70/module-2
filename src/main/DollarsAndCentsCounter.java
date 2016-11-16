344b2dbfc017675aaccbf3c573baca01e1306fcc
Simon Struck <simonstruck70@gmail.com>
2016-11-16 14:38:43 +0100

    Renamed Dollars (again)

--- src/main/DollarsAndCentsCounter.java
+++ src/main/DollarsAndCentsCounter.java
@@ -1,20 +1,49 @@
 package main;
 
-
 public class DollarsAndCentsCounter {
-    public int cents() {
 
-    }
+	private int	dollars;
+	private int	cents;
 
-    public void add(int dollars, int cents) {
+	public int dollars() throws Exception {
+		if (this.dollars >= 0) {
+			return this.dollars;
+		} else {
+			throw new Exception("DollarsAndCentsCounter is negative");
+		}
+	}
 
-    }
+	public int cents() throws Exception {
+		if (this.cents < 0) {
+			throw new Exception("Cents are negative");
+		} else if (this.cents > 99) {
+			throw new Exception("Cents is too big");
+		}
+		return this.cents;
+	}
 
-    public void reset() {
+	public void add(int dollars, int cents) {
+		this.dollars += dollars;
+		this.cents += cents;
 
-    }
+		while (this.cents > 100) {
+			this.dollars += 1;
+			this.cents -= 100;
+		}
+	}
 
-    public void substract(int dollars, int cents) {
+	public void reset() {
+		this.dollars = 0;
+		this.cents = 0;
+	}
 
-    }
+	public void substract(int dollars, int cents) {
+		this.dollars -= dollars;
+		this.cents -= cents;
+		
+		while (this.cents < 0) {
+			this.dollars -= 1;
+			this.cents += 100;
+		}
+	}
 }
--- test/CounterAndCentsCounterTest.java
+++ test/CounterAndCentsCounterTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.assertEquals;
  * @author Jip Spel
  * @version $Revision: 1.0 $
  */
-public class DollarsAndCentsCounterTest {
+public class CounterAndCentsCounterTest {
     /** Testvariabele for a <tt>DollarsAndCentsCounter</tt> object. */
     private DollarsAndCentsCounter counter;
 
