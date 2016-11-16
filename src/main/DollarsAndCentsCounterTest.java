dc52489b7e826f4c5d060a8a4371dde9ec01270e
Simon Struck <simonstruck70@gmail.com>
2016-11-16 14:09:21 +0100

    Rename Dollars

--- src/main/DollarsAndCentsCounterTest.java
+++ src/main/DollarsAndCentsCounterTest.java
@@ -26,7 +26,7 @@ public class DollarsAndCentsCounterTest {
     }
 
     /**
-     * Test the method <tt>dollars()</tt>
+     * Test the method <tt>Dollars()</tt>
      */
     @Test
     public void testDollars() {
@@ -48,7 +48,7 @@ public class DollarsAndCentsCounterTest {
     }
 
     /**
-     * Test the method <tt>add(dollars, cents)</tt>
+     * Test the method <tt>add(Dollars, cents)</tt>
      */
     @Test
     public void testAdd() {
