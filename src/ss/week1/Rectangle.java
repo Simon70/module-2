2c5721855dbb36916b2f04eaf517bfe235596f78
WeersProductions <weersproductions@gmail.com>
2016-11-21 16:02:31 +0100

    Added Rectangle.java

---
+++ src/ss/week1/Rectangle.java
@@ -0,0 +1,36 @@
+package ss.week1;
+
+
+public class Rectangle {
+	private int length;
+	private int width;
+	private int area;
+	private int perimeter;
+	
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+	}
+
+	
+	public int getLength() {
+		return length;
+	}
+
+	
+	public int getWidth() {
+		return width;
+	}
+
+	
+	public int getArea() {
+		return area;
+	}
+
+	
+	public int getPerimeter() {
+		return perimeter;
+	}
+	
+	
+}
