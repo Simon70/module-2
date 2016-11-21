2606b5c466a1d60f28ace9530aa96e204cc5793c
WeersProductions <weersproductions@gmail.com>
2016-11-21 17:18:17 +0100

    Added Safe

--- src/ss/week2/hotel/Safe.java
+++ src/ss/week2/hotel/Safe.java
@@ -1,7 +1,44 @@
 package ss.week2.hotel;
 
-
 public class Safe {
-	
-	
+
+	private /*@ spec_public @*/ String	password;
+	private /*@ spec_public @*/ Boolean	active;
+	private /*@ spec_public @*/ Boolean open;
+
+	//@ assignable active;
+	public void activate(String password) {
+		if (password.equals(this.password)) {
+			active = true;
+		}
+	}
+
+	//@ assignable open;
+	//@ assignable active;
+	public void deactivate() {
+		open = false;
+		active = false;
+	}
+
+	//@ assignable open;
+	public void open(String password) {
+		if (active && password.equals(this.password)) {
+			open = true;
+		}
+	}
+
+	//@ assignable open;
+	public void close() {
+		open = false;
+	}
+
+	//@ pure
+	public Boolean isActive() {
+		return active;
+	}
+
+	//@ pure
+	public Boolean isOpen() {
+		return open;
+	}
 }
