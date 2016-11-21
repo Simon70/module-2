7ff7740d6f558b1fb8585560f01a963dd8928045
Simon Struck <simonstruck70@gmail.com>
2016-11-21 16:43:40 +0100

    Added RectangleTest

---
+++ src/ss/week2/test/RectangleTest.java
@@ -0,0 +1,16 @@
+package ss.week2.test;
+
+import org.junit.Test;
+import ss.week2.Rectangle;
+
+
+/**
+ * Created by simon on 21.11.16.
+ */
+public class RectangleTest {
+    @Test
+    public void test() {
+        Rectangle testRectangle = new Rectangle(10,10);
+
+    }
+}
