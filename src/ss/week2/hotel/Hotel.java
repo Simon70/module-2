cb6bfaa0797b97853bc72c71695e99ac5a8c661d
Simon Struck <simonstruck70@gmail.com>
2016-11-24 10:22:42 +0100

    Implemented Hotel Fixed Safe

--- src/ss/week2/hotel/Hotel.java
+++ src/ss/week2/hotel/Hotel.java
@@ -5,28 +5,59 @@ package ss.week2.hotel;
  */
 public class Hotel {
 
+    private String name;
+    private Room room0;
+    private Room room1;
 
-    private final String name;
-
-    public Hotel(String name){
+    public Hotel(String name) {
         this.name = name;
+        this.room0 = new Room(1, new Safe());
+        this.room1 = new Room(2, new Safe());
     }
 
     public Room checkIn(String password, String guestName) {
-        Room room = 
+        Room room;
+        if ((room = getFreeRoom()) == null)
+            return null;
+        room.getSafe().activate(password);
+        if (!room.getSafe().isActive())
+            return null;
+        Guest guest = new Guest(guestName);
+        guest.checkin(room);
+        return room;
     }
 
     public void checkOut(String guestName) {
+        Room room = getRoom(guestName);
+        if (room != null) {
+            room.getSafe().deactivate();
+            room.getGuest().checkout();
+        }
     }
 
     public Room getRoom(String guestName) {
+
+        if (room0.getGuest() != null && room0.getGuest().getName().equals(guestName))
+            return room0;
+        if (room1.getGuest() != null && room1.getGuest().getName().equals(guestName))
+            return room1;
+        return null;
     }
 
     public Password getPassword() {
-        return password;
+        return new Password();
     }
 
-
     public Room getFreeRoom() {
+        if (room0.getGuest() == null)
+            return room0;
+        if (room1.getGuest() == null)
+            return room1;
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel: [Room 0=" + room0.toString() + ", Room 1=" + room1.toString() + "]";
     }
 }
--- src/ss/week2/hotel/Safe.java
+++ src/ss/week2/hotel/Safe.java
@@ -2,43 +2,50 @@ package ss.week2.hotel;
 
 public class Safe {
 
-	private /*@ spec_public @*/ String	password;
-	private /*@ spec_public @*/ Boolean	active;
-	private /*@ spec_public @*/ Boolean open;
+    private /*@ spec_public @*/ Password password;
+    private /*@ spec_public @*/ Boolean active;
+    private /*@ spec_public @*/ Boolean open;
 
-	//@ assignable active;
-	public void activate(String password) {
-		if (password.equals(this.password)) {
-			active = true;
-		}
-	}
+    public Safe() {
+        password = new Password();
+        active = false;
+        open = false;
+    }
 
-	//@ assignable open;
-	//@ assignable active;
-	public void deactivate() {
-		open = false;
-		active = false;
-	}
 
-	//@ assignable open;
-	public void open(String password) {
-		if (active && password.equals(this.password)) {
-			open = true;
-		}
-	}
+    //@ assignable active;
+    public void activate(String password) {
+        if (this.password.testWord(password)) {
+            active = true;
+        }
+    }
 
-	//@ assignable open;
-	public void close() {
-		open = false;
-	}
+    //@ assignable open;
+    //@ assignable active;
+    public void deactivate() {
+        open = false;
+        active = false;
+    }
 
-	//@ pure
-	public Boolean isActive() {
-		return active;
-	}
+    //@ assignable open;
+    public void open(String password) {
+        if (active && this.password.testWord(password)) {
+            open = true;
+        }
+    }
 
-	//@ pure
-	public Boolean isOpen() {
-		return open;
-	}
+    //@ assignable open;
+    public void close() {
+        open = false;
+    }
+
+    //@ pure
+    public Boolean isActive() {
+        return active;
+    }
+
+    //@ pure
+    public Boolean isOpen() {
+        return open;
+    }
 }
