1c91299de8abbabb460044d1b92e9ae5ce9f84cf
WeersProductions <weersproductions@gmail.com>
2016-12-08 12:17:41 +0100

    Completed HomeWork during signoff

--- src/ss/week4/math/Homework.java
+++ src/ss/week4/math/Homework.java
@@ -9,25 +9,27 @@ public class Homework {
 		System.out.println("f(x) = " + f1 + ", f(8) =  "+f1.apply(8));
 		System.out.println("f(x) = " + f2 + ", f(8) =  "+f2.apply(8));
 		System.out.println("f(x) = " + f3 + ", f(8) =  "+f3.apply(8));
-		
+
 		Polynomial polynomial = new Polynomial(new double[] { 9.45, 5.23, 3.25, 1.23 });
 		f2 = polynomial.integrand();
 		f3 = polynomial.derivative();
 		System.out.println("f(x) = " + polynomial + ", f(8) =  "+polynomial.apply(8));
 		System.out.println("f(x) = " + f2 + ", f(8) =  "+f2.apply(8));
 		System.out.println("f(x) = " + f3 + ", f(8) =  "+f3.apply(8));
-		
+
 
 		Product f4 = new Product(new Exponent(4), f1);
 		f3 = f4.derivative();
 		System.out.println("f(x) = " + f4 + ", f(8) =  "+f4.apply(8));
 		System.out.println("f(x) = " + f3 + ", f(8) =  "+f3.apply(8));
-		
+
 		Constant f5 = new Constant(10);
 		f2 = f5.integrand();
 		f3 = f5.derivative();
 		System.out.println("f(x) = " + f5 + ", f(8) =  "+f5.apply(8));
 		System.out.println("f(x) = " + f2 + ", f(8) =  "+f2.apply(8));
 		System.out.println("f(x) = " + f3 + ", f(8) =  "+f3.apply(8));
+
+
 	}
 }
