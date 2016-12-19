783aa234a1e3ea905c2c9ed2b58d678322216cb9
Simon Struck <simonstruck70@gmail.com>
2016-12-19 15:59:45 +0100

    Merge branch 'master' of https://github.com/Simon70/module-2 into dev-simon
    # Conflicts:
    #	src/ss/project/shared/game/Engine.java
    #	src/ss/project/shared/game/World.java

--- src/ss/week5/tictactoe/Strategy.java
+++ src/ss/week5/tictactoe/Strategy.java
@@ -4,7 +4,11 @@ package ss.week5.tictactoe;
  * Created by simon on 13.12.16.
  */
 public interface Strategy {
+    //@ pure
+    //@ ensures this.getName().isEmpty() == false
     public String getName();
 
+    //@ pure
+    //@ requires b.
     public int determineMove(Board board, Mark mark);
 }
