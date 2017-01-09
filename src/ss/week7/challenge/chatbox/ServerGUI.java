1bceb293c7452c7e3da735e7e7180f992192b5f5
WeersProductions <weersproductions@gmail.com>
2017-01-09 15:36:42 +0100

    Imported week7

--- src/ss/week7/challenge/chatbox/ServerGUI.java
+++ src/ss/week7/challenge/chatbox/ServerGUI.java
@@ -1,3 +1,23 @@
+package ss.week7.challenge.chatbox;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 public class ServerGUI extends JFrame implements ActionListener, MessageUI {
 	private JButton bConnect;
 	private JTextField tfPort;
@@ -65,7 +85,7 @@ public class ServerGUI extends JFrame implements ActionListener, MessageUI {
 		cc.add(p2);
 	}
 
-	/** returns the Internetadress of this computer */
+	/** returns the Internetadress of this computer. */
 	private String getHostAddress() {
 		try {
 			InetAddress iaddr = InetAddress.getLocalHost();
@@ -76,7 +96,7 @@ public class ServerGUI extends JFrame implements ActionListener, MessageUI {
 	}
 
 	/**
-	 * listener for the "Start Listening" button
+	 * listener for the "Start Listening" button.
 	 */
 	public void actionPerformed(ActionEvent ev) {
 		Object src = ev.getSource();
@@ -108,12 +128,12 @@ public class ServerGUI extends JFrame implements ActionListener, MessageUI {
 		addMessage("Started listening on port " + port + "...");
 	}
 
-	/** add a message to the textarea  */
+	/** add a message to the textarea.  */
 	public void addMessage(String msg) {
 		taMessages.append(msg + "\n");
 	}
 
-	/** Start a ServerGUI application */
+	/** Start a ServerGUI application. */
 	public static void main(String[] args) {
 		new ServerGUI();
 	}
